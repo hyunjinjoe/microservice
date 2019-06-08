dac36cab8f4689dd7cb93fc8e91adf471f9a7e22
hyunjinjoe <jini@daum.net>
2019-06-09 08:20:57 +0900

    Update MicroserviceApplication.java

--- src/main/java/com/msa/spring/MicroserviceApplication.java
+++ src/main/java/com/msa/spring/MicroserviceApplication.java
@@ -20,6 +20,6 @@ public class MicroserviceApplication {
     public String hello( @PathVariable("firstName") String firstName,
                          @PathVariable("lastName") String lastName) {
 
-        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+        return "Hello, " + firstName + " " + lastName;
     }
 }
